8e2358963a10c530fbce9f2c66eabdc06338200c
pater97 <giuseppepater997@gmail.com>
2022-01-14 14:54:32 +0100

    creazione tabella in remoto (bug: errore nel trasfermento tab al database)

---
+++ database/migrations/2022_01_14_123722_create_travels_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination');
+            $table->string('departure from');
+            $table->date('date');
+            $table->int('price');
+            $table->string('stay time');
+            $table->string('hotel');
+            $table->string('treatment');
+            $table->int('number of people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
